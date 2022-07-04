f66f0c85224353bfa0f10eff96a8557af1704267
Riccardo <lupinu.ric@gmail.com>
2022-07-04 15:21:05 +0200

    Project: Fine (Da Revisionare)

--- README.md
+++ README.md
@@ -4,3 +4,17 @@
 * Autore: Lupinu Riccardo
 * Data: 04/07/2022
 * Linguaggio: HTML, JavaScript
+
+# Algoritmo:
+
+Chiedere all'utente numero chilometri
+Chiedere all'utente età del passeggero
+Moltiplicare il numero di chilometri per la tariffa base di un singolo chilometro
+IF L'eta inserita è uguale a 0 o maggiore di 105 
+    Messaggio di errore
+ELSE IF L'età è minore di 18
+    Applicare sconto del 20%
+ELSE IF L'età è maggiore o uguale a 65
+    Applicare sconto del 40%
+ELSE
+    Pagare il biglietto a prezzo intero
---
+++ main.js
@@ -0,0 +1,28 @@
+const kilometriDaFare = parseInt(prompt("Inserisci i chilometri che vuoi percorrere:" ));
+const eta = parseInt(prompt("Inserisci la tua età:" ));
+const prezzoKilometro = 0.21;
+
+let prezzoIntero = (kilometriDaFare * prezzoKilometro);
+prezzoIntero = prezzoIntero.toFixed(2);100
+let percentualeMinorenne = (20 / 100);
+let percentualeOver = (40 / 100);
+
+
+if (eta === 0 || eta > 105) {
+    alert("Inserisci un età valida e riprova!")
+}
+else if (eta < 18) {
+    let scontoMinorenni = (percentualeMinorenne * prezzoIntero);
+    let prezzoMinorenni = (prezzoIntero - scontoMinorenni);
+    prezzoMinorenni = prezzoMinorenni.toFixed(2);
+    alert("Hai diritto al 20%  di sconto. Pagherai " + prezzoMinorenni + "€");
+} 
+else if (eta >= 65) {
+    let scontoOver = (percentualeOver * prezzoIntero);
+    let prezzoOver = (prezzoIntero - scontoOver)
+    prezzoOver = prezzoOver.toFixed(2);
+    alert("Hai diritto al 40%  di sconto. Pagherai " + prezzoOver + "€");
+}
+else {
+    alert("Non hai diritto allo sconto. Pagherai " + prezzoIntero + "€");
+}
